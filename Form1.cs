1f19d647b656055db4b7232c37c08fea02249ec2
MateuszBizior <m_bizior@o2.pl>
2017-06-11 11:39:42 +0200

    Wysyłanie i odbieranie danych cs

--- Form1.cs
+++ Form1.cs
@@ -13,31 +13,93 @@ namespace WifiCOM
 {
     public partial class Form1 : Form
     {
-
-        public static SerialPort SerialPort1 = new System.IO.Ports.SerialPort("COM3");
+        string InputData = string.Empty;
+        delegate void SetTextCallback(string text);
 
         public Form1()
         {
 
             InitializeComponent();
+            AvaliablePorts();
         }
 
         private void SerialReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
         {
-            ReciveBox.Text = serialPort1.ReadExisting();
+            try
+            {
+                InputData = serialPort1.ReadTo("n");
+                if (InputData != String.Empty)
+                {
+                    SetText(InputData);
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("Błąd podczas odbierania");
+            }
+        }
+
+
+        void SetText(string text)
+        {
+            if (this.ReciveBox.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.BeginInvoke(d, new object[] { text });
+            }
+            else
+            {
+                ReciveBox.Text = text;
+            }
+        }
+
+
+        void AvaliablePorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            PortsCbx.Items.AddRange(ports);
+
         }
 
         private void SendBtn_Click(object sender, EventArgs e)
         {
-            
+            try
             {
-                SerialPort1.Write(SendBox.Text);
+                if(serialPort1.IsOpen)
+                serialPort1.Write(SendBox.Text);
                 
             }
-            
+            catch(UnauthorizedAccessException)
             {
-                
+                MessageBox.Show("Błąd podczas wysłania danych!!\n Upewnij się czy wybrany port jest dostępny.");
             }
+        }
+
+        private void ConectBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if( PortsCbx.Text != "" && BaudRateCbx.Text!="")
+                {
+                    serialPort1.PortName = PortsCbx.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(BaudRateCbx.Text);
+                    serialPort1.Open();
+                }
+                else
+                {
+                    MessageBox.Show("Ustaw właściwości portu COM");
+                }
+            }
+            catch(UnauthorizedAccessException)
+            {
+                MessageBox.Show("Błąd połączenia");
+            }
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
         }
     }
 }
